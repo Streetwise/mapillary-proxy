df26af3998553bb21632372f2609ef87369b3f63
Oleg Lavrovsky <oleg@datalets.ch>
2022-06-11 23:39:05 +0200

    Caching

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import requests
 import os
 
 from wsgiref.simple_server import make_server
+from falcon_caching import Cache
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -14,7 +15,14 @@ except KeyError:
     print("Environment not ready: see README for required keys")
     exit(1)
 
+cache = Cache(
+    config={
+        'CACHE_EVICTION_STRATEGY': 'time-based',  # how records are evicted
+        'CACHE_TYPE': 'simple'  # what backend to use to store the cache
+    })
 
+
+@cache.cached(timeout=600)
 class ThumbResource:
 
     def on_get(self, req, resp):
@@ -24,11 +32,18 @@ class ThumbResource:
         if not len(imgkey) > 5:
             resp.text = 'key parameter is required'
             return
+        imgurl = cache.get(imgkey)
+        if imgurl:
+            raise falcon.HTTPMovedPermanently(
+                location=imgurl
+            )
         url = "https://graph.mapillary.com/" + \
             "%s?access_token=%s&fields=thumb_256_url" % (imgkey, APP_TOKEN)
+        print("Fetching image thumbnail")
         r = requests.get(url)
         if r.json().get('thumb_256_url'):
             imgurl = r.json().get('thumb_256_url')
+            cache.set(imgkey, imgurl)
             raise falcon.HTTPMovedPermanently(
                 location=imgurl
             )
@@ -38,7 +53,7 @@ class ThumbResource:
             )
 
 
-app = application = falcon.App()
+app = application = falcon.App(middleware=cache.middleware)
 app.add_route('/thumb', ThumbResource())
 
 
